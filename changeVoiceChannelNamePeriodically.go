f30c2ba0d5c5417aa8a778ccc36dcb9c2b5abffa
Miguel Machado <hello@miguelmachado.dev>
2023-04-07 00:20:05 -0300

    fix: change ticker to minute

--- changeVoiceChannelNamePeriodically.go
+++ changeVoiceChannelNamePeriodically.go
@@ -19,7 +19,7 @@ func changeVoiceChannelName(s *discordgo.Session, channelID string, newName stri
 }
 
 func ChangeVoiceChannelNamePeriodically(s *discordgo.Session, channelID string, intervalMinutes int) {
-	ticker := time.NewTicker(time.Duration(intervalMinutes) * time.Second)
+	ticker := time.NewTicker(time.Duration(intervalMinutes) * time.Minute)
 	defer ticker.Stop()
 
 	for range ticker.C {
